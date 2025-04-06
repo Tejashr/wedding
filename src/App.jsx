c5b4458ca58b348dacfb4a6561141f7ba332d92b
Tejashr <tejasteju11@gmail.com>
2025-04-06 20:21:26 +0530

    initialize React app with Vite, add basic structure and styles

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="save-the-date-container">
+        <h1 className="couple-names">Shiva Kumar & Rakshitha </h1>
+        <h1 className="couple-names">Welcome You </h1>
+
+        {/* <h4 className="save-the-date">Save the Date</h4> */}
+        <div className="event-dates">
+          <p className="event">Reception: May 18, 8 PM</p>
+          <p className="event">Marriage: May 19, 10 AM</p>
+        </div>
+        <div className="map-link">
+          <a
+            href="https://maps.app.goo.gl/Robpfux4FRWYGUE67?g_st=aw"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            View Convention Hall on Map
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
